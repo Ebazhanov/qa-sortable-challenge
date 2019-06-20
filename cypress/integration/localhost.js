4bc53e147fd4df8281d25b0ba4472cb3923fccaf
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-06-20 22:45:24 +0200

    cypress-example

--- cypress/integration/localhost.js
+++ cypress/integration/localhost.js
@@ -3,14 +3,8 @@ context('Aliasing', () => {
         cy.visit('/')
     });
 
-    xit('.as() - alias a DOM element for later use', () => {
+    it('.as() - alias a DOM element for later use', () => {
         cy.get('.as-table').find('tbody>tr')
-            .first().find('td').first()
-            .find('button').as('firstBtn');
-        cy.get('@firstBtn').click();
-        cy.get('@firstBtn')
-            .should('have.class', 'btn-success')
-            .and('contain', 'Changed')
     });
 
     it('should run test sample', () => {
